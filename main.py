961c70ed043232c3e2967a568a0d0ee2f3c4eca5
akeeme <akeemeblack@gmail.com>
2021-03-05 22:27:33 -0500

    $splits, $trend working // rapidapi

--- main.py
+++ main.py
@@ -3,22 +3,31 @@ from discord.ext import commands
 import os
 import yfinance as yf
 import asyncio
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import pandas as pd
 from datetime import date
+# from matplotlib import rcParams
+import requests
+# from pandas.io.json import json_normalize
+import numpy as np
 
 token = os.getenv('token')
+rKey = os.getenv('rKey')
+rHost = os.getenv('rHost')
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
         await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name = "the Stock Market"))
-        print('Logged in')
+        print('Logged in as', client.user.name)
+
     
     async def on_message(self, message: discord.message.Message):
-      # async with message.typing():
-      #   await asyncio.sleep(2)
-      if message.author.id != client.user.id:
-        if message.content.startswith('$s'):
 
+
+      if message.author.id != client.user.id:
+        if message.content.startswith('$stock'):
           stock = message.content.split(' ')[1].upper()
           ticker = yf.Ticker(stock)
           s_high = ticker.info.get("dayHigh")
@@ -26,59 +35,130 @@ class MyClient(discord.Client):
           s_url = ticker.info.get("website")
           s_name = ticker.info.get("longName")
           s_logo = ticker.info.get("logo_url")
-
+          prev_close = ticker.info.get("previousClose")
 
           embed=discord.Embed(
           title= stock,
               url=s_url,
               description=f"Stock info for {s_name}" ,
               color=discord.Color.purple())
-          embed.set_author(name=f"{s_name}", url=f"{s_url}", icon_url=f"{s_logo}")
+          embed.set_author(name=f"{s_name}", url=f"{s_url}", icon_url=f"{s_logo}")     
           embed.add_field(name="**Day High**", value=f"${s_high}", inline=False)
           embed.add_field(name="**Day Low**", value=f"${s_low}", inline=False)
-          #embed.add_field(name="**Options**", value=f"${opt}", inline=False)
+          embed.add_field(name = "**Previous Close**", value = f"${prev_close}", inline = False)
+
           await message.channel.send(embed = embed)
 
-        if message.content.startswith('$op'):
-          # today = date.today()
-          # td_day = today.strftime("%Y/%m/%d")
+        if message.content.startswith('$split'):
           stock = message.content.split(' ')[1].upper()
           ticker = yf.Ticker(stock)
           s_url = ticker.info.get("website")
           s_name = ticker.info.get("longName")
           s_logo = ticker.info.get("logo_url")
-          opt = ticker.options
+          splits = ticker.splits
           embed = discord.Embed(
           title = stock,
             url = s_url,
-            description = f"Options info for {s_name}",
+            description = f"Splits for {s_name}",
             color = discord.Color.purple())
           embed.set_author(name = f"{s_name}", url = f"{s_url}", icon_url = f"{s_logo}")
-          embed.add_field(name = "**Option Dates**", value = f"{opt}")
+          embed.add_field(name = "**Splits**", value = f"{splits}")
 
           await message.channel.send(embed = embed)
         
-        if message.content.startswith('$chain'):
-          stock = message.content.split(' ')[1].upper()
-          opDate = message.content.split(' ')[2]
-          ticker = yf.Ticker(stock)
-          s_url = ticker.info.get("website")
-          s_name = ticker.info.get("longName")
-          s_logo = ticker.info.get("logo_url")
-          opt = ticker.option_chain(opDate)
-          embed = discord.Embed(
-          title = stock,
-            url = s_url,
-            description = f"Options Chain for {s_name}",
-            color = discord.Color.purple())
-          embed.set_author(name = f"{s_name}", url = f"{s_url}", icon_url = f"{s_logo}")
-          embed.add_field(name = "**Options**", value = f"{opt}")
+        # dividends not working right now (api's fault)
 
 
+        # if message.content.startswith('$div'):
+        #   stock = message.content.split(' ')[1].upper()
+        #   ticker = yf.Ticker(stock)
+        #   s_url = ticker.info.get("website")
+        #   s_name = ticker.info.get("longName")
+        #   s_logo = ticker.info.get("logo_url")
+        #   div = ticker.dividends
+        #   print("div started")
+        #   embed = discord.Embed(
+        #   title = stock,
+        #     url = s_url,
+        #     description = f"Dividends for {s_name}",
+        #     color = discord.Color.purple())
+        #   embed.set_author(name = f"{s_name}", url = f"{s_url}", icon_url = f"{s_logo}")
+        #   embed.add_field(name = "**Dividends**", value = f"{div}")
+
+        if message.content.startswith('$trend'):
+          url="https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-trending-tickers"
+
+
+          querystring = {"region":"US"}
+          
+        
+          headers = {
+            'x-rapidapi-key': rKey,
+            'x-rapidapi-host': rHost
+    }
+          response = requests.request("GET", url, headers=headers, params=querystring)
+
+          r2 = response.json()
+
+          # stonk 1
+          
+          q1 = r2["finance"]['result'][0]['quotes'][0]
+          stonk1 = q1['symbol']
+          stock1_market_price = q1['regularMarketPrice']
+          s1MCP = q1['regularMarketChangePercent']
+          stock1_market_change_per = (float(np.round(s1MCP, 2)))
+        
+
+
+          # stonk 2
+          q2 = r2["finance"]['result'][0]['quotes'][1]
+          stonk2 = q2['symbol']
+          stock2_market_price = q2['regularMarketPrice']
+          s2MCP = q2['regularMarketChangePercent']
+          stock2_market_change_per = (float(np.round(s2MCP, 2)))
           
 
 
-          # opt = stock.option_chain(td_day)
+          # stonk 3
+          q3 = r2["finance"]['result'][0]['quotes'][2]
+          stonk3 = q3['symbol']
+          stock3_market_price = q3['regularMarketPrice']
+          s3MCP = q3['regularMarketChangePercent']
+          stock3_market_change_per = (float(np.round(s3MCP, 2)))
+          
+
+          # stonk 4
+          q4 = r2["finance"]['result'][0]['quotes'][3]
+          stonk4 = q4['symbol']
+          stock4_market_price = q4['regularMarketPrice']
+          s4MCP = q4['regularMarketChangePercent']
+          stock4_market_change_per = (float(np.round(s4MCP, 2)))
+          
+
+
+          # stonk 5
+          q5 = r2["finance"]['result'][0]['quotes'][4]
+          stonk5 = q5['symbol']
+          stock5_market_price = q5['regularMarketPrice']
+          s5MCP = q5['regularMarketChangePercent']
+          stock5_market_change_per = (float(np.round(s5MCP, 2)))
+          
+          
+
+          embed = discord.Embed(
+          title = "Trending Tickers",
+          description = "This is a list of the current top 5 trending tickers in the stock market. (Information from Yahoo Finance)" ,
+            color = discord.Color.purple())
+
+
+          embed.add_field(name = "Stock", value=f"{stonk1} \n {stonk2} \n {stonk3} \n {stonk4} \n {stonk5}", inline = True)
+
+          embed.add_field(name = "Price", value = f"${stock1_market_price} \n ${stock2_market_price} \n ${stock3_market_price} \n ${stock4_market_price} \n ${stock5_market_price}", inline = True)
+
+          embed.add_field(name = "Change", value = f"{stock1_market_change_per}% \n {stock2_market_change_per}% \n {stock3_market_change_per}% \n {stock4_market_change_per}% \n {stock5_market_change_per}%", inline = True)
+
+          await message.channel.send(embed = embed)
+
 
 
         
@@ -95,12 +175,7 @@ class MyClient(discord.Client):
             description = "$s (ticker) - gives the Day high and Day low for a stock**" + "\n \n" + "**$help or $commands - gives list of commands",
             color = discord.Color.purple())
           await message.channel.send (embed = embed)
-        if message.content.startswith('$q'):
-          async with message.typing():
-            await asyncio.sleep(2)
-          await message.channel.send("typing test")
-
-          
+      
           
 
 
